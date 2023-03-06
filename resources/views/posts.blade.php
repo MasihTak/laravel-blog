6c7f0359b5240b62bce66dc1c7b1d6a5c9a15d0c
MasihTak <masihtak@outlook.com>
2023-03-06 21:41:36 +0100

    refactor: add posts view

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My blog</title>
+</head>
+<body>
+    <article>
+        <h1>My first Post</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque consectetur, dignissimos dolorem doloribus facere, harum iste magni maiores, molestias officiis placeat quaerat quas quidem voluptas. Iure nostrum quo tenetur?</p>
+    </article>
+    <article>
+        <h1>My second Post</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dignissimos distinctio ducimus ea expedita harum impedit ipsam iste laboriosam magni maxime mollitia numquam, officia quaerat rerum, tempore tenetur, unde voluptas!</p>
+    </article>
+    <article>
+        <h1>My third Post</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque consectetur, dignissimos dolorem doloribus facere, harum iste magni maiores, molestias officiis placeat quaerat quas quidem voluptas. Iure nostrum quo tenetur?</p>
+    </article>
+</body>
+</html>
