7cdb8f1c077ccc27cd85572a946f0538ab02b543
MasihTak <masihtak@outlook.com>
2023-05-28 00:43:48 +0200

    refactor: layout component

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,4 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+<x-layout>
     @foreach ($posts as $post)
         <article>
             <a href="/posts/{{ $post->slug }}">
@@ -19,5 +10,4 @@
             </div>
         </article>
     @endforeach
-</body>
-</html>
+</x-layout>
