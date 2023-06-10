767f3f2b0de71b11e7fc236e4278f18ceb2bab1d
MasihTak <masihtak@outlook.com>
2023-06-10 22:09:51 +0200

    fix: display categories in views

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,9 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
 }
