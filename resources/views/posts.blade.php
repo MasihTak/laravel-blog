2722049157962c8b383de46ff8b60653be2e621d
MasihTak <masihtak@outlook.com>
2023-06-11 17:48:45 +0200

    feat: display posts by category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
             </a>
 
             <p>
-                <a href="#">{{ $post->category->name }}</a>
+                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
             </p>
 
             <div>
